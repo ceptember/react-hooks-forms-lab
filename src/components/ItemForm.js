c03324d4464fe0dc806734b02f9bb4c9a5411430
Christy Perozzi <christy.perozzi@gmail.com>
2022-01-04 20:36:03 -0500

    This seems to work but it won't pass one of the tests

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,35 @@
 import React from "react";
-import { v4 as uuid } from "uuid";
+import { v4 as uuid } from "uuid"; //Assignment came with this imported here
+
+// Test failed - says it expects onItemFormSubmit to be called with the new item object
+// I think that means I have to create the new item in ItemForm component and pass it up to ShoppingList 
+
+//put an empty new item in the state of the parent
+//pass it to the child as a prop
+//Also pass the function as a prop
+// Create the new item in the child component
+// But how to pass it back up to the parent? Can it go as an argument in the callback?  
+
 
 function ItemForm(props) {
+
+  const newItem = {
+    id: uuid(), 
+    name: props.newName,
+    category: props.newCategory,
+  };
+
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit ={(event) => props.onItemFormSubmit(event, newItem)} >
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={props.newName} onChange={props.onNameChange}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={props.newCategory} onChange={props.onCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,24 +2,54 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+//import { v4 as uuid } from "uuid"; // I added this. Assignmnent came with uuid in ItemForm
 
 function ShoppingList({ items }) {
+  //State for the Filter component
   const [selectedCategory, setSelectedCategory] = useState("All");
-  const [searchedItem, setSearchedItem] = useState("")
+  const [searchedItem, setSearchedItem] = useState(""); 
 
+  //State for the ItemForm component
+  const [newName, setNewName] = useState("");
+  const [newCategory, setNewCategory] = useState("Produce");
+  const [itemsArr, setItemsArr] = useState(items);
+
+  //Set new state for the Filter category  
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
+  //Set new state for the Filter search  
   function handleSearchChange(event){
     setSearchedItem(event.target.value); 
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  //Set new state for the ItemForm new item name
+  function handleNewName(event){
+    setNewName(event.target.value);
+    
+  };
+
+  //Set new state for the ItemForm new item category 
+  function handleNewCategory(event){
+    setNewCategory(event.target.value);
+  };
+
+  //Sumbit new item from ItemForm
+  function handleNewItemSubmit(event, submittedItem){
+    event.preventDefault(); 
+    setItemsArr([...itemsArr, submittedItem])
+    setNewName("");
+    setNewCategory("Produce")
+  }
+
+
+  // Items for the list 
+  const itemsToDisplay = itemsArr.filter((item) => {
     if (selectedCategory === "All" && searchedItem === "") 
       {return true}
     else if (searchedItem){
-      return item.name === searchedItem; 
+      return item.name.includes(searchedItem); 
     }  
     else {
       return item.category === selectedCategory;
@@ -29,8 +59,12 @@ function ShoppingList({ items }) {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} searchedItem={searchedItem}/>
+      <ItemForm onItemFormSubmit = {handleNewItemSubmit} 
+                onNameChange = {handleNewName} 
+                onCategoryChange ={handleNewCategory} 
+                newName={newName} 
+                newCategory={newCategory}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} search={searchedItem}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
